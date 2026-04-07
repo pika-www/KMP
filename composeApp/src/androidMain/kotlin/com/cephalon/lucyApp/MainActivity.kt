3f393b4cb4de14ac5c80152dfc070de4e644d767
Roy <roy@cephalon.ai>
2026-04-07 17:04:19 +0800

    fix: 修复安卓页面上面部分黑框

--- composeApp/src/androidMain/kotlin/com/cephalon/lucyApp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/cephalon/lucyApp/MainActivity.kt
@@ -3,13 +3,11 @@ package com.cephalon.lucyApp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import com.cephalon.lucyApp.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             App()
         }
