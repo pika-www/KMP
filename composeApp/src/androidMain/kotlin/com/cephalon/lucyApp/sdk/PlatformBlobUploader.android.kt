3fc874dc0c5b315297393b42905327a84f8c0d47
Roy <roy@cephalon.ai>
2026-04-13 10:37:43 +0800

    chore: update submodule

--- composeApp/src/androidMain/kotlin/com/cephalon/lucyApp/sdk/PlatformBlobUploader.android.kt
+++ composeApp/src/androidMain/kotlin/com/cephalon/lucyApp/sdk/PlatformBlobUploader.android.kt
@@ -1,9 +1,15 @@
 package com.cephalon.lucyApp.sdk
 
+import com.cephalon.lucyApp.screens.agentmodel.AndroidAppContextHolder
 import lucy.im.sdk.blob.BlobPutResult
 import lucy.im.sdk.blob.BlobTransfer
+import java.io.File
 
-private val blobTransfer = BlobTransfer()
+private val blobTransfer by lazy {
+    val dir = File(AndroidAppContextHolder.appContext.filesDir, "lucy_blob")
+    BlobTransfer.initStoreDir(dir.absolutePath)
+    BlobTransfer()
+}
 
 internal actual suspend fun platformUploadBlob(data: ByteArray, entryName: String): BlobPutResult {
     return blobTransfer.put(data, entryName)
